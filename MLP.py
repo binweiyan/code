ead529a12a37b8d17c0655385a2a39c973023f13
binweiyan <bineva@mit.edu>
2023-07-19 21:35:43 -0400

    1

---
+++ MLP.py
@@ -0,0 +1,9 @@
+#arg parse
+parser = argparse.ArgumentParser(description='MLP')
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--layer_num', type=int, default=1)
+parser.add_argument('--hidden_size', type=int, default=128)
+parser.add_argument('--dropout', type=float, default=0.5)
+parser.add_argument('--batch_norm', type=bool, default=False)
