345d6fb80f5f283ae971f511db4803fee5731a17
binweiyan <bineva@mit.edu>
2023-07-19 21:36:30 -0400

    1

--- MLP.py
+++ MLP.py
@@ -1,3 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import argparse
+
 #arg parse
 parser = argparse.ArgumentParser(description='MLP')
 parser.add_argument('--batch_size', type=int, default=128)
@@ -7,3 +13,5 @@ parser.add_argument('--layer_num', type=int, default=1)
 parser.add_argument('--hidden_size', type=int, default=128)
 parser.add_argument('--dropout', type=float, default=0.5)
 parser.add_argument('--batch_norm', type=bool, default=False)
+
+args = parser.parse_args()
