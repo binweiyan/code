4f799be9ceb7defbd4985b8d674413f666128008
Binwei Yan <bineva@mit.edu>
2024-09-16 20:44:50 -0400

    1

--- tradebot.py
+++ tradebot.py
@@ -107,24 +107,28 @@ class fastertradebot(tradebot):
 risk_limit_df = pd.DataFrame(columns = idx, index = ["limit"])
 risk_limit_df.loc["limit"] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
 
+
+import scipy.optimize as opt
+
+
 class fullbot(tradebot):
     def __init__(self, lamb, fr, Cov, grosslimit = 5e7, netlimit = 1e7):
         super().__init__(lamb, fr, Cov)
         self.grosslimit = grosslimit
         self.netlimit = netlimit
     def trade(self, alpha, price):
-        x = cp.Variable(len(alpha))
-        objective = 0.5 * self.lamb * cp.quad_form(x, self.Cov) + (self.lamb * np.dot(self.Cov, self.pos)) @ x - alpha * self.fr @ x
-        abs_terms = cp.sum(cp.abs(np.multiply(self.pos, x)))
-        objective += abs_terms
-        #limit the gross and net position, calculated in risk_limit_df
-        constraint = [cp.sum(cp.abs(cp.multiply(market_width_df.loc["width"] / price, x))) <= self.grosslimit, cp.sum(cp.abs(cp.multiply(market_width_df.loc["width"] / price, x))) <= self.netlimit]
-        problem = cp.Problem(cp.Minimize(objective), constraint)
-        problem.solve()
-        #check if cash is enough
-        if np.dot(price, x.value) > self.cash:
-            x.value = x.value * self.cash / np.dot(price, x.value)
-        #check if 
+        #x proportional to fr * alpha
+        #but the proportion is limited by the risk limit: 
+        # risk_limit_df / price * (x + pos) < netlimit
+        # abs(risk_limit_df / price * (x + pos)) < grosslimit
+        def obj(x):
+            return 0.5 * self.lamb * x @ self.Cov @ x + self.lamb * np.dot(self.Cov, self.pos) @ x - alpha @ self.fr @ x + np.sum(np.abs(np.multiply(x, market_width_df.loc["width"] / price)))
+        def cons(x):
+            return [self.netlimit - np.abs(np.sum(np.multiply(risk_limit_df.loc["limit"] / price, x + self.pos))), self.grosslimit - np.sum(np.abs(np.multiply(risk_limit_df.loc["limit"] / price, x + self.pos)))]
+        x0 = np.zeros(len(alpha))
+
+        res = opt.minimize(obj, x0, constraints = {"type": "ineq", "fun": cons})
+        x = res.x
         self.pos += x.value
         self.cash -= np.dot(price, x.value) + np.sum(np.abs(np.multiply(market_width_df.loc["width"] / price, x.value)))
         self.update(price)
@@ -145,6 +149,7 @@ class fullbot(tradebot):
         plt.figure()
         if endtime == -1:
             endtime = len(df)
+        pnls = []
         for i in range(starttime, endtime):
             price = df.loc[i][idx]
             self.update(price)
@@ -152,7 +157,7 @@ class fullbot(tradebot):
             self.trade(alpha, price)
             #generate pnl graph
             pnl = self.cash + np.sum(self.pos)
-            plt.scatter(i, pnl)
+            pnls.append(pnl)
             if i % 100 == 0:
                 plt.show()
-        plt.show()
+        plt.plot(range(len(pnls)), pnls, label = "pnl")
